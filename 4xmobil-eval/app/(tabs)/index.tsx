9178cbe219977584b73b9f2f7d139d55121d9a83
thanh680 <nguyenthanhdanh680@gmail.com>
2026-01-14 12:31:03 +0100

    corrige inscription

--- 4xmobil-eval/app/(tabs)/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Platform, StyleSheet } from 'react-native';
-import { Redirect } from 'expo-router';
-
-export default function HomeScreen() {
-  return (
-    <Redirect href="/(tabs)/offreur" />
-  );
-}
